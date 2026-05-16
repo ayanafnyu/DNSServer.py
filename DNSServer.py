cb1cbdb044f5dc271fe799ce474d447d734a8472
ayanafnyu <af5462@nyu.edu>
2026-05-16 01:39:52 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -43,7 +43,7 @@ def decrypt_with_aes(encrypted_data, password, salt):
     decrypted_data = f.decrypt(encrypted_data) #call the Fernet decrypt method
     return decrypted_data.decode('utf-8')
 
-salt = b'Tandon') # Remember it should be a byte-object
+salt = b'Tandon' # Remember it should be a byte-object
 password = 'aar651@nyu.edu'
 input_string = 'AlwaysWatching'
 
@@ -144,7 +144,7 @@ def run_dns_server():
 
             # Send the response back to the client using the `server_socket.sendto` method and put the response to_wire(), return to the addr you received from
             print("Responding to request:", qname)
-            server_socket.sendto(resonse.to_wire(), addr)
+            server_socket.sendto(response.to_wire(), addr)
         except KeyboardInterrupt:
             print('\nExiting...')
             server_socket.close()
